bb841795587f1379ee192866f60f9183d189e074
Ashiq Ahamed <aashiq121205@gmail.com>
2024-09-14 02:08:06 +0530

    update

--- alter.js
+++
@@ -1,7 +0,0 @@
-module.exports.item = ['red bull', 'ferrari', 'merc'];
-
-const person = {
-    name: 'MAX',
-};  
-
-module.exports.singlePer = person; 
--- grenade.js
+++
@@ -1,8 +0,0 @@
-const n1 = 5;
-const n2 = 10;
-
-const add = () => {
-    console.log(`sum is ${n1 + n2}`);
-}
-console.log(`hiii`);
-add();
