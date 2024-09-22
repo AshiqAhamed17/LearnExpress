1a2b311e9019358237422a70a4b478588051c1f2
Ashiq Ahamed <aashiq121205@gmail.com>
2024-09-22 15:02:18 +0530

    update index.js

--- Basic/index.js
+++ Basic/index.js
@@ -19,7 +19,7 @@ app.get('/products', (req, res) =>{
      const {category, minPrice, maxPrice, sortBy } = req.query;
      let items = [
         { name: 'MacBook M3 Pro', category: 'Electronics', price: 3499 },
-        { name: 'LV-shirt', category: 'Clothing', price: 500 },
+        { name: 'LV-shirt', category: 'Clothing', price: 549 },
         { name: 'i-phone 16', category: 'Electronics', price: 999 },
         { name: 'Gucci', category: 'Clothing', price: 600 }
     ];
